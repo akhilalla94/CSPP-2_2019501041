86290fb5db23fd027fb5ab1a161e6c483642648c
akhilalla94 <akhilalla1994@msitprogram.net>
2019-09-24 18:39:30 +0530

    COMPLETED

--- MODULE2/ASSIGNMENT10/TestRunner.java
+++ MODULE2/ASSIGNMENT10/TestRunner.java
@@ -1,6 +1,8 @@
 /**
- * This is TestRunner class which picks the test cases from the JUnit and executes 
- * each test case. This will shows an output on the console about how many test cases
+ * This is TestRunner class which picks the test cases from the JUnit
+ * and executes each test case.
+ * 
+ * This will shows an output on the console about how many test cases
  * passed / failed when you run this TestRunner class.
  * 
  * There will be a detailed information about which test cases have been failed 
@@ -26,20 +28,16 @@ public class TestRunner {
          System.out.println("Oh!!! Some of the test cases failed may be because of logical errors");
          System.out.println("Please see the details below for the test cases that are failed");
          System.out.println("===============================");
-         try {
-            for (Failure failure : result.getFailures()) {
-               int index = failure.getDescription().toString().indexOf("(");
-               System.out.println("Test Case : " + failure.getDescription().toString().substring(8, index));
-               int start = failure.getMessage().indexOf("<");
-               int end = failure.getMessage().indexOf(">");
-               System.out.println("Expected Output : " + failure.getMessage().substring(start+1, end));
-               start = failure.getMessage().indexOf("<", start + 1);
-               end = failure.getMessage().indexOf(">", end + 1);
-               System.out.println("Your Output : " + failure.getMessage().substring(start+1, end));
-               System.out.println("===============================");
-            }
-         } catch(Exception ex) {
-            ex.printStackTrace();
+         for (Failure failure : result.getFailures()) {
+            int index = failure.getDescription().toString().indexOf("(");
+            System.out.println("Test Case : " + failure.getDescription().toString().substring(8, index));
+            int start = failure.getMessage().indexOf("<");
+            int end = failure.getMessage().indexOf(">");
+            System.out.println("Expected Output : " + failure.getMessage().substring(start+1, end));
+            start = failure.getMessage().indexOf("<", start + 1);
+            end = failure.getMessage().indexOf(">", end + 1);
+            System.out.println("Your Output : " + failure.getMessage().substring(start+1, end));
+            System.out.println("===============================");
          }
          System.out.println((result.getRunCount() - result.getFailureCount()) + " / " + result.getRunCount() + " Test Cases Passed....");
       }
---
+++ main1.java
@@ -0,0 +1,13 @@
+class main1{
+    public static void main(String[]args){
+        int i=1;
+        do{
+            System.out.println(i);
+            i=i+4;
+        }while(i<=10);
+                
+           
+           
+        }
+    
+    }
